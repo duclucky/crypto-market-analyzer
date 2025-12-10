cd07aa6627f5c4f36b18f21a848b069501f93a68
Your Name <your.email@gmail.com>
2025-12-10 08:18:06 +0700

    utils: Add logging configuration

---
+++ src/utils/logger.py
@@ -0,0 +1,17 @@
+﻿"""Logging configuration."""
+
+import logging
+
+def setup_logger(name, level=logging.INFO):
+    """Configure logger for the application."""
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    
+    return logger
