4441a9561a604044c0e48f80aa4c77609bb36614
Your Name <your.email@gmail.com>
2025-12-10 08:18:06 +0700

    utils: Add data processor utilities

---
+++ src/utils/data_processor.py
@@ -0,0 +1,19 @@
+﻿"""Data processing utilities."""
+
+class DataProcessor:
+    """Process and normalize market data."""
+    
+    @staticmethod
+    def normalize_data(data):
+        """Normalize market data to 0-1 range."""
+        pass
+    
+    @staticmethod
+    def calculate_statistics(data):
+        """Calculate mean, std, min, max."""
+        pass
+    
+    @staticmethod
+    def filter_outliers(data, threshold=3):
+        """Remove statistical outliers."""
+        pass
