5a8a9bcbc818523757547364b5e0482a5f7af3b2
Your Name <your.github.email@gmail.com>
2025-12-10 15:06:41 +0700

    utils: Add data processor utilities

--- src/utils/data_processor.py
+++ src/utils/data_processor.py
@@ -5,15 +5,4 @@ class DataProcessor:
     
     @staticmethod
     def normalize_data(data):
-        """Normalize market data to 0-1 range."""
-        pass
-    
-    @staticmethod
-    def calculate_statistics(data):
-        """Calculate mean, std, min, max."""
-        pass
-    
-    @staticmethod
-    def filter_outliers(data, threshold=3):
-        """Remove statistical outliers."""
         pass
